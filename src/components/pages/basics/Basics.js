542855b6ced717f2026fdf16bffa2c969ee67bf4
Suman Barick <codotronix@gmail.com>
2020-11-09 12:10:40 +0530

    added simple logical cards

--- src/components/pages/basics/Basics.js
+++ src/components/pages/basics/Basics.js
@@ -1,10 +1,37 @@
+import { useState } from 'react'
 import DecBinRow from './DecBinRow'
 
 const Basics = props => {
+    const [numA, setNumA] = useState(2)
+    const [numB, setNumB] = useState(7)
+
     return (
         <div>
-            <DecBinRow name="A" />
-            <DecBinRow name="B" />
+            <DecBinRow 
+                name="A" 
+                dec={numA} 
+                onChange={setNumA} 
+            />
+            <DecBinRow 
+                name="B" 
+                dec={numB} 
+                onChange={setNumB} 
+            />
+            <DecBinRow 
+                name="A & B" 
+                dec={numA&numB} 
+                // onChange={setNumB} 
+            />
+            <DecBinRow 
+                name="A | B" 
+                dec={numA|numB} 
+                // onChange={setNumB} 
+            />
+            <DecBinRow 
+                name="A ^ B" 
+                dec={numA^numB} 
+                // onChange={setNumB} 
+            />
         </div>
     )
 }
--- src/components/pages/basics/DecBinRow.js
+++ src/components/pages/basics/DecBinRow.js
@@ -1,42 +1,45 @@
-import { useState } from 'react'
-
-const initVal = {
-    dec: 0,
-    bin: '00000000'
-}
+import clsx from 'clsx'
 
 const DecBinRow = props => {
-    const { name } = props
-    const [value, setValue] = useState(initVal)
+    const { name, dec, onChange } = props
 
+    /**
+     * Whenever user types in decimal field.
+     * @param {*} e 
+     */
     const onChangeDec = e => {
-        const dec = parseInt(e.target.value)
-        if(isNaN(dec)) return
+        const newDec = parseInt(e.target.value)
+        if(isNaN(newDec)) return
 
-        else if(dec < 0 || dec > 255) return alert('The decimal number must be in the range 0 <= n <= 255')
+        else if(newDec < 0 || newDec > 255) return alert('The decimal number must be in the range 0 <= n <= 255')
 
-        else {
-            const bin = converDecToBin8bitString(dec)
-            setValue({ dec, bin })
-        }
+        else onChange && onChange(newDec)
     }
 
+    /**
+     * Whenever user changes in one of bin fields
+     * @param {*} e | event
+     * @param {*} i | Index of the field corresponding to binary array
+     */
     const onChangeBin = (e, i) => {
         const val = parseInt(e.target.value)
         if( isNaN(val) || val < 0 || val > 1 ) return alert('A binary digit must be either 0 or 1')
 
-        let bin = value.bin.split('')
+        let bin = converDecToBin8bitString(dec).split('')
         bin[i] = val
         bin = bin.join('')
 
-        const dec = parseInt(bin, 2)
-        setValue({ dec, bin })
+        const newDec = parseInt(bin, 2)
+        onChange && onChange(newDec)
     }
 
+    /**
+     * Binary Shift Operator
+     * @param {*} direction | 'LEFT' or 'RIGHT'
+     */
     const handleShift = direction => {
-        const dec = direction === 'LEFT' ? value.dec << 1 : value.dec >> 1
-        const bin = converDecToBin8bitString(dec)
-        setValue({ dec, bin })
+        const newDec = direction === 'LEFT' ? dec << 1 : dec >> 1
+        onChange && onChange(newDec)
     }
 
     /**
@@ -44,7 +47,7 @@ const DecBinRow = props => {
      * it will return an 8 bit binary string
      * @param {*} n 
      */
-    const converDecToBin8bitString = n => n.toString(2).padStart(8, '0')
+    const converDecToBin8bitString = n => n.toString(2).padStart(8, '0').substr(0, 8)
 
     return (
         <div className="row dec-bin-row shadow">
@@ -55,14 +58,14 @@ const DecBinRow = props => {
                     <input type="text" 
                         className="form-control" 
                         id={'decimal'+name} 
-                        value={value.dec} 
+                        value={dec} 
                         onChange={onChangeDec}
                     />
                 </div>
             </div>
             <div className="col-sm-12 col-md-8 d-flex binaries">
                 {
-                    value.bin.split('').map( (b, i) =>
+                    converDecToBin8bitString(dec).split('').map( (b, i) =>
                     <div className="form-group mr-2" key={i}>
                         <label htmlFor={`bits-${name}-${7-i}`}>2<sup>{7-i}</sup></label>
                         <input 
@@ -77,7 +80,7 @@ const DecBinRow = props => {
                 }
                 
             </div>
-            <div className="col-sm-12 btn-panel">
+            <div className={clsx("col-sm-12 btn-panel", !onChange && 'd-none')}>
                 <button 
                     type="button" 
                     className="btn btn-primary" 
